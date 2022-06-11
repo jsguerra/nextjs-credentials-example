22129caed70bb48f639aa116c6020e4588c7b4c6
jsguerra <jose@gcostudios.com>
2022-06-11 13:10:39 +0100

    updated logic

--- src/pages/private.tsx
+++ src/pages/private.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const PrivatePage = () => {
-  return <h1>PrivatePage</h1>;
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div style={{ maxWidth: "960px", margin: "auto" }}>
+        <h1>Private Page</h1>
+        <div>
+          {session ? (
+            <>
+              <p>Private Content</p>
+              <p>
+                <button onClick={() => signOut()}>Sign Out</button>
+              </p>
+            </>
+          ) : (
+            <>
+              <p>You are not permitted to see this page.</p>
+              <p>
+                <button onClick={() => signIn()}>Sign in</button>
+              </p>
+            </>
+          )}
+          <p>
+            <Link href="/">
+              <a>Return Home</a>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PrivatePage;
